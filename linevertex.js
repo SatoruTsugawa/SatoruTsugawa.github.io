4f209943e5379999013c19c17ffdbcdfa961386c
SatoruTsugawa <69041483+SatoruTsugawa@users.noreply.github.com>
2025-08-29 07:58:37 +0900

    Add files via upload

--- linevertex.js
+++ linevertex.js
@@ -244,7 +244,7 @@ function sliderSetting(){
   textgamma.style('font-size','20px');
   textTitle.style('font-weight','bold');
 
-  sliderEPS = createSlider(1, 6, 3);
+  sliderEPS = createSlider(1, 16, 10);
   sliderEPS.position(sliderLeftEnd, sliderYposition+kankaku*2);
   sliderEPS.style('width', sliderLength);
   textEPS=createP('Elasto-gravi length &#949(g/B)');
